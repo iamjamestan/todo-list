4258f3692aefb7292cb37d3e5de45045c764d9af
iamjamestan <jamestanyuli@gmail.com>
2019-12-30 15:29:46 +0800

    added ability to mark todo items as complete, and added a bit of styling to show the complete status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_065350) do
+ActiveRecord::Schema.define(version: 2019_12_30_071212) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_12_30_065350) do
     t.integer "todo_list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
   end
 
